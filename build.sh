46eb076874e71447271d396e10f758e7de78e43b
kmaitreys <peakcipher1729@gmail.com>
2024-05-16 22:16:50 +0530

    add compiler optimization flag

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p build
 cd build
-f2py -c ../lib/_dlsodes.pyf --opt='-std=legacy' -m ../lib/blkdta000.f ../lib/opkda1.f ../lib/opkda2.f ../lib/dlsodes.f
+f2py -c ../lib/_dlsodes.pyf --opt='-std=legacy -O3' -m ../lib/blkdta000.f ../lib/opkda1.f ../lib/opkda2.f ../lib/dlsodes.f

99f5c7ccbc5c353f8e6cb3faddfb9c7b6189e250
Maitrey <53518181+kmaitreys@users.noreply.github.com>
2024-05-21 10:11:25 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ class CustomInstallCommand(install):
 
 setup(
     name="pylsodes",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     package_data={
         "pylsodes": ["_dlsodes.pyi"] + [file.split("/")[-1] for file in so_files]
